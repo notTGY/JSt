e173c91b75c3b0a35616243d58962f5ae79dab60
notTGY <microcraft47@gmail.com>
2020-11-20 13:35:25 +0300

    release 1

--- release/1/index.js
+++ release/1/index.js
@@ -3,6 +3,10 @@ let img = document.querySelector('#icon');
 let title = document.querySelector('#name_of_product');
 let pageOne = document.querySelector('#slide_one');
 let pageTwo = document.querySelector('#slide_two');
+let pageThree = document.querySelector('#slide_three');
+let pageFour = document.querySelector('#slide_four');
+let pageFive = document.querySelector('#slide_five');
+let pageSix = document.querySelector('#slide_six');
 
 onload = _ => {
 
@@ -13,17 +17,32 @@ onload = _ => {
 	imgInitialWidth = img.width;
 	imgInitialHeight = img.height;
 
+	document.body.style.background = 'radial-gradient(#000, #151515)';
+
 
 	title.style.opacity = 0;
 	title.hidden = true;
 	img.style.opacity = 1;
 	img.hidden = false;
+	
 	pageOne.hidden = true;
 	pageOne.style.opacity = 0;
+	
 	pageTwo.style.opacity = 0;
 	pageTwo.hidden = true;
+	
+	pageThree.style.opacity = 0;
+	pageThree.hidden = true;
+	
+	pageFour.style.opacity = 0;
+	pageFour.hidden = true;
 
-}
+	pageFive.style.opacity = 0;
+	pageFive.hidden = true;
+
+	pageSix.style.opacity = 0;
+	pageSix.hidden = true;
+}	
 let currentDelta = 0;
 
 
@@ -63,28 +82,91 @@ window.addEventListener('wheel', e=>{
 		pageOne.style.opacity = 0;
 	}
 
-	if (currentDelta > 100 && currentDelta < 150) {
+	if (currentDelta > 100 && currentDelta < 130) {
 		pageOne.hidden = false;
 		pageOne.style.opacity = 1;
-	} else if (currentDelta > 150){
+	} else if (currentDelta > 130){
 		pageOne.hidden = true;
 		pageOne.style.opacity = 0;
 	}
 
 
-	if (currentDelta > 150 && currentDelta < 170) {
-		pageTwo.style.opacity = (currentDelta-150)/20;
+	if (currentDelta > 130 && currentDelta < 150) {
+		pageTwo.style.opacity = (currentDelta-130)/20;
 		pageTwo.hidden = false;
 	} else if (currentDelta < 150){
 		pageTwo.hidden = true;
 		pageTwo.style.opacity = 0;
 	}
 
-	if (currentDelta > 170 && currentDelta < 220) {
+	if (currentDelta > 150 && currentDelta < 180) {
 		pageTwo.hidden = false;
 		pageTwo.style.opacity = 1;
-	} else if (currentDelta > 220){
+	} else if (currentDelta > 180){
 		pageTwo.hidden = true;
 		pageTwo.style.opacity = 0;
+	}
+
+	if (currentDelta > 180 && currentDelta < 200) {
+		pageThree.style.opacity = (currentDelta-180)/20;
+		pageThree.hidden = false;
+	} else if (currentDelta < 200){
+		pageThree.hidden = true;
+		pageThree.style.opacity = 0;
+	}
+
+	if (currentDelta > 200 && currentDelta < 230) {
+		pageThree.hidden = false;
+		pageThree.style.opacity = 1;
+	} else if (currentDelta > 230){
+		pageThree.hidden = true;
+		pageThree.style.opacity = 0;
+	}
+
+	if (currentDelta > 230 && currentDelta < 250) {
+		pageFour.style.opacity = (currentDelta-230)/20;
+		pageFour.hidden = false;
+	} else if (currentDelta < 250){
+		pageFour.hidden = true;
+		pageFour.style.opacity = 0;
+	}
+
+	if (currentDelta > 250 && currentDelta < 280) {
+		pageFour.hidden = false;
+		pageFour.style.opacity = 1;
+	} else if (currentDelta > 280){
+		pageFour.hidden = true;
+		pageFour.style.opacity = 0;
+	}
+
+	if (currentDelta > 280 && currentDelta < 300) {
+		pageFive.style.opacity = (currentDelta-280)/20;
+		pageFive.hidden = false;
+		document.body.style.background = '#FFF';
+	} else if (currentDelta < 280){
+		pageFive.hidden = true;
+		pageFive.style.opacity = 0;
+		document.body.style.background = 'radial-gradient(#000, #151515)';
+	}
+
+	if (currentDelta > 300 && currentDelta <= 330) {
+		pageFive.hidden = false;
+		pageFive.style.opacity = 1;
+	} else if (currentDelta >= 330){
+		pageFive.hidden = true;
+		pageFive.style.opacity = 0;
+	}
+
+	if (currentDelta > 330 && currentDelta < 350) {
+		pageSix.style.opacity = (currentDelta-330)/20;
+		pageSix.hidden = false;
+	} else if (currentDelta < 330){
+		pageSix.hidden = true;
+		pageSix.style.opacity = 0;
+	}
+
+	if (currentDelta > 350) {
+		pageSix.hidden = false;
+		pageSix.style.opacity = 1;
 	}
 });

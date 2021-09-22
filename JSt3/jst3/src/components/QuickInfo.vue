b07a9a777845bf0afab8649ed17d0156eb393486
notTGY <microcraft47@gmail.com>
2021-09-22 13:14:33 +0300

    release frontend with vue!!!

---
+++ JSt3/jst3/src/components/QuickInfo.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="bg">
+  QuickInfo
+  <Card text="hi"/>
+  </div>
+</template>
+
+<script>
+import Card from './Card.vue'
+export default {
+  name: 'QuickInfo',
+  components: {
+    Card,
+  }
+}
+</script>
+
+<style scoped>
+.bg {
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: radial-gradient(#333, #555);
+  color: lightblue;
+}
+</style>
